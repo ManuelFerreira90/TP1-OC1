faa06423b35b17150505857a2eec478dd22c4deb
Ítallo Winícios <italloferreirainfo@gmail.com>
2023-04-12 19:43:03 -0300

    Menu de escolhas de arquivo

--- Projeto/main.py
+++ Projeto/main.py
@@ -1,13 +1,22 @@
+import os
+
 def lerArquivo(arquivo):
 
     try:
-        with open("Projeto/Arquivos_teste/"+arquivo) as a:
-            conteudo = a.read()
+        with open("Projeto/Arquivos_teste/"+arquivo) as arq:
+            conteudo = arq.read()
+            print("\nConteudo:")
             print(conteudo)
+
     except FileNotFoundError:
         msg = "Infelizmente, o arquivo " + arquivo + " não existe!"
         print(msg)
         
 
-arquivo = input("Digite o nome do arquivo teste: ")
+pasta = "Projeto/Arquivos_teste/"
+files = os.listdir(pasta) 
+print("Arquivos de teste: \n", files)
+
+num = int(input("\nDigite qual arquivo teste deseja: "))       
+arquivo = files[num]
 lerArquivo(arquivo)
