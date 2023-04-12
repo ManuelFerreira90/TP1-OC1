c9e1d750f6e30145ad167c37ea7a11280bb2729a
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2023-04-12 20:32:04 -0300

    variável para armazenar as linhas da entrada

--- Projeto/main.py
+++ Projeto/main.py
@@ -1,7 +1,5 @@
 #lw sw sub xor addi srl beq
 
-
-
 import os
 
 def lerArquivo(arquivo):
@@ -10,8 +8,13 @@ def lerArquivo(arquivo):
         with open("Projeto/Arquivos_teste/"+arquivo) as arq:
             texto = arq.readlines()
             print("\nConteudo:")
+            #variavel para armzenar cada linha do comando em assembly
+            x = ''
             for linha in texto :
                 print(linha)
+                x = linha.split(" ")
+                #x = x.strip()
+                print(x)
             arq.close()
 
     except FileNotFoundError:
