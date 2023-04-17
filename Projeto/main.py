441d3cbbc1303e0d762e768a3f1bc40a96e2cdf7
Ítallo Winícios <italloferreirainfo@gmail.com>
2023-04-17 11:26:02 -0300

    Menu inicial

--- Projeto/main.py
+++ Projeto/main.py
@@ -3,11 +3,26 @@ import os
 import functions.ler_arquivo as func
 import functions.montador as mont
 
-pasta = "Projeto/Arquivos_teste/"
-files = os.listdir(pasta) 
-print("Arquivos de teste: \n", files)
+num = 0
+while(num != 9):
+    print("\n1 = Ler arquivo;\n"
+        "2 = Inserir instrucoes;\n"
+        "9 = Encerrar.\n")
+    num = int(input("Digite uma opcao: "))
 
-num = int(input("\nDigite qual arquivo teste deseja: "))       
-arquivo = files[num]
-func.lerArquivo(arquivo)
+    #Limpar tela do terminal
+    os.system('clear')
+    if num == 1:
+        pasta = "Projeto/Arquivos_teste/"
+        files = os.listdir(pasta) 
+        print("Arquivos de teste: \n", files)
+        num = int(input("\nDigite qual arquivo teste deseja: "))       
+        arquivo = files[num]
+        func.lerArquivo(arquivo)
 
+    elif num == 2:
+        instrucao = input("Digite sua instrucao assembly: ")
+    elif num == 9:
+        print("Encerrando...")
+    else:
+        print("ERRO: opcao invalida!")
